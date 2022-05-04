77abd7d056dda8ce233fba2b94636313264619ca
geminoa <geminoa@gmail.com>
2022-05-05 02:36:28 +0900

    Introduce cmd subdir
    Move rootCmd to another file `cmd/root.go`.
    
    Signed-off-by: geminoa <geminoa@gmail.com>

---
+++ cmd/root.go
@@ -0,0 +1,44 @@
+package cmd
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	rootCmd = &cobra.Command{
+		Use:   "yadate",
+		Short: "Yet another date command",
+		Long: `Yet another date command for providing more flexible ways for ` +
+			`standard date command.`,
+		Run: func(cmd *cobra.Command, args []string) {
+			if bUtc, err := cmd.Flags().GetBool("utc"); err == nil {
+				if bUtc {
+					tz := "America/New_York"
+					a, err := time.LoadLocation(tz)
+					if err != nil {
+						panic(err)
+					}
+					now := time.Now().In(a)
+					fmt.Println(now)
+				} else {
+					now := time.Now()
+					fmt.Println(now)
+				}
+			}
+		},
+	}
+)
+
+func Execute() error {
+	rootCmd.Flags().BoolP(
+		"utc", "u", false,
+		"print or set Coordinated Universal Time (UTC)")
+	rootCmd.Flags().StringP(
+		"date", "d", "",
+		"display time described by STRING, not 'now'")
+
+	return rootCmd.Execute()
+}
--- main.go
+++ main.go
@@ -1,40 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"time"
-
-	"github.com/spf13/cobra"
-)
+import "github.com/geminoa/yadate/cmd"
 
 func main() {
-	tz := "America/New_York"
-	a, err := time.LoadLocation(tz)
-
-	if err != nil {
-		panic(err)
-	}
-
-	var rootCmd = &cobra.Command{
-		Use:   "yadate",
-		Short: "Yet another date command",
-		Long:  `Yet another date command for providing more flexible ways for standard date command.`,
-		Run: func(cmd *cobra.Command, args []string) {
-			if bUtc, err := cmd.Flags().GetBool("utc"); err == nil {
-				if bUtc {
-					now := time.Now().In(a)
-					fmt.Println(now)
-				} else {
-					now := time.Now()
-					fmt.Println(now)
-				}
-			}
-		},
-	}
-
-	rootCmd.Flags().BoolP(
-		"utc", "u", false,
-		"print or set Coordinated Universal Time (UTC)")
-
-	rootCmd.Execute()
+	cmd.Execute()
 }
